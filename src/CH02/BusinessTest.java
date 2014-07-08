e307030a5effb521b80d2c9e6aba5e8a58d9bcc5
KangUk <hesangel@nate.com>
2014-07-08 10:45:55 +0900

    #1 나도수정함

--- src/CH02/BusinessTest.java
+++ src/CH02/BusinessTest.java
@@ -1,11 +1,11 @@
-package CH02;
+﻿package CH02;
 
 public class BusinessTest {
 	Business business;
 	
 	public static void main(String[] args){
 		BusinessA executee = new BusinessA();
-		executee.insert("강욱", "핳핳", 100);
+		executee.insert("강욱", "핳핳", 1200);
 	}
 	
 }
